4579c613bdfa38afb636063c3af3881a2c4f074e
Ahmedgit7 <131698234+Ahmedgit7@users.noreply.github.com>
2024-08-26 10:35:18 +0530

    Update backend.tf

--- Terraformcode/backend.tf
+++ Terraformcode/backend.tf
@@ -1,11 +1,11 @@
-# # backend.tf
+# backend.tf
 
-# terraform {
-#   backend "s3" {
-#     bucket         = "netflixterrbackend"
-#     key            = "terraform/state.tfstate"  # Path to the state file inside the S3 bucket
-#     region         = "ap-south-1"               # Your AWS region
-#     encrypt        = true                       # State file should be encrypted
-#     dynamodb_table = "terraform-locks"          # DynamoDB table for state locking
-#   }
-# }
+terraform {
+   backend "s3" {
+     bucket         = <yourbucketname>
+     key            = "terraform/state.tfstate"  # Path to the state file inside the S3 bucket
+     region         = "ap-south-1"               # Your AWS region
+     encrypt        = true                       # State file should be encrypted
+     dynamodb_table = <dynamodb-table>          # DynamoDB table for state locking
+   }
+ }
